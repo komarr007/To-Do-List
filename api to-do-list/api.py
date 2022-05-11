546de045a6f9496e2eee7af8a7e94fe46e7a17ee
komarr007 <mariorangga000@gmail.com>
2022-05-11 11:17:57 +0700

    cleaning api structure

--- api to-do-list/api.py
+++ api to-do-list/api.py
@@ -6,6 +6,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    return {"message": "Hello World"}
+
+@app.get("/data")
+async def data():
     dbname = mongo_connect.get_database('testing')
     collection_name = dbname['to_do_list']
     return mongo_connect.shows_data(collection_name, {"Tittle":"Tittle1"},{'_id':0})
