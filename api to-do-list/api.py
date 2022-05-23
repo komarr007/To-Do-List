bb242d5eec3b3f1ef66e11ae28474d9e9125d549
michaelseptiadi <septiadimichael@gmail.com>
2022-05-23 15:49:08 +0700

    tarik data

--- api to-do-list/api.py
+++ api to-do-list/api.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import mongo_connect
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,4 +23,3 @@ async def data():
     dbname = mongo_connect.get_database('testing')
     collection_name = dbname['to_do_list']
     return mongo_connect.shows_data(collection_name, {"Tittle":"Tittle1"},{'_id':0})
-    
